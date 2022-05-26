1d2d044578ee3612e4de6dada10434827f29f1c6
Ueric <31016216+Ueric@users.noreply.github.com>
2022-05-26 14:36:56 -0300

    Update map.R

--- scripts/map.R
+++ scripts/map.R
@@ -1,1 +1,30 @@
 
+cat("\014") ## clear RStudio console
+rm(list=ls()) ## clear the Global Environment
+
+
+library(ggplot2)
+library(dplyr)
+library(viridis)
+library(brazilmaps)
+library(geobr)
+library(sf)
+library(maptools)
+library(leaflet)
+library(geojsonio)
+library(gpclib)
+library(dplyr)
+library(broom)
+
+#Brazilian map
+
+mapa <- read_state(showProgress = FALSE)
+
+class(mapa)
+mapa
+ggplot(mapa)+ 
+  geom_sf() +
+  theme_map() +
+  
+
+ggsave(file="brazilmap.pdf")
