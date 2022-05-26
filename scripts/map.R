d1f139b9d78112a545e93eb282b7652185fbd987
Ueric <31016216+Ueric@users.noreply.github.com>
2022-05-26 14:50:04 -0300

    Update map.R

--- scripts/map.R
+++ scripts/map.R
@@ -38,3 +38,48 @@ ggsave(file="brazilmap.pdf")
 
 #Tocantins map
 
+TO_map <- geojson_read("Tocantins.geojson", what = "sp")
+TO_map
+plot(TO_map)
+TO_map_fortified <- tidy(TO_map, region = "id")
+TO_map_fortified
+ggplot()+
+  geom_polygon(data = TO_map_fortified, aes(x = long, y = lat, group = group), fill = "white", color="grey") +
+  theme_void() + 
+  coord_map()
+
+data <- read.table(file = "n.txt", header = T, sep = "\t")
+data
+data$id_commom <- as.character(data$id_commom)
+is.character(data$id_commom)
+
+spdf_fortified = spdf_fortified %>% 
+  left_join(. , data, by=c("id" = "id_commom"))
+
+final_map_TO <- ggplot() +
+  geom_polygon(data = TO_map_fortified, aes(fill = N, x = long, y = lat, group = group),  color="black", size=0.1, alpha=0.9) +
+  theme_void() +
+  scale_fill_gradient(low = "#fee6ce", high = "#a63603", na.value = "grey100", 
+                      trans = "log", breaks=c(1,2,12)) +
+  #scale_fill_continuous(na.value = "grey70", trans = "log", breaks=c(0,83), name="Number of sequenced genomes", guide = guide_legend( keyheight = unit(3, units = "mm"), keywidth=unit(12, units = "mm"), label.position = "bottom", title.position = 'top', nrow=1) ) +
+  #labs(
+  #  title = "",
+  #  subtitle = "",
+  #  caption = "") +
+  theme(
+    text = element_text(color = "#22211d"),
+    plot.background = element_rect(fill = "#ffffff", color = NA),
+    panel.background = element_rect(fill = "#ffffff", color = NA),
+    legend.background = element_rect(fill = "#ffffff", color = NA),
+    #plot.title = element_text(size= 12, hjust = 0.5, vjust = 0.5, color = "black"),
+    #plot.subtitle = element_text(size= 12, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.43, l = 2, unit = "cm")),
+    #plot.caption = element_text( size=12, color = "#4e4d47", margin = margin(b = 0.3, r=-99, unit = "cm") ),
+    
+    legend.position = "bottom"
+  ) +
+  coord_map()
+
+final_map_TO
+ggsave(file="TOmap.pdf", width=6, height=5.5, dpi=600)
+
+#Final editing in inkscape (https://inkscape.org/pt-br/)
