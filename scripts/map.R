af0aa80b6ed25cecb87b8e99e07bffaa34dcbd7c
Ueric <31016216+Ueric@users.noreply.github.com>
2022-05-26 14:38:11 -0300

    Update map.R

--- scripts/map.R
+++ scripts/map.R
@@ -1,8 +1,15 @@
+# R Code for tree visualization
+# 
+# This code is part of, and published under the same terms and conditions as, the following publication 
+# Ueric José Borges de Souza, Raíssa Nunes dos Santos, Marta Giovanetti, Luiz Carlos Junior Alcantara, Jucimária Dantas Galvão, 
+# Franciano Dias Pereira Cardoso, Feliph Cássio Sobrinho Brito, Ana Cláudia Franco, Paulo Michel Roehe, Bergmann Morais Ribeiro,
+# Fernando Rosado Spilki, Fabricio Souza Campos
+# (2022). Genomic epidemiology reveals the circulation of the chikungunya virus East-Central South African lineage in Tocantins state, North Brazil. 
+# Submitted in . 
 
+### load required libraries and import data
 cat("\014") ## clear RStudio console
 rm(list=ls()) ## clear the Global Environment
-
-
 library(ggplot2)
 library(dplyr)
 library(viridis)
@@ -28,3 +35,6 @@ ggplot(mapa)+
   
 
 ggsave(file="brazilmap.pdf")
+
+#Tocantins map
+
